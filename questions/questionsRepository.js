a47a887f551900aeb3c9cc44758587621b7dfe24
Lelkes Balázs <balazs.lelkes89@gmail.com>
2021-05-18 11:37:33 +0200

    add questions controller + repository

---
+++ questions/questionsRepository.js
@@ -0,0 +1,22 @@
+import db from '../db.js';
+
+export default class QuestionsRepository {
+
+  static collection = db.collection('questions');
+
+  static async getAll() {
+    let queryRef = await QuestionsRepository.collection.get();
+    const questions = [];
+    queryRef.forEach(doc => questions.push({ id: doc.id, ...doc.data() }));
+    return questions;
+  }
+
+  static async add(doc) {
+    const result = await QuestionsRepository.collection.add(doc);
+    return result;
+  }
+
+  static async remove(id) {
+    await QuestionsRepository.collection.doc(id).delete();
+  }
+}
