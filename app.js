be2dc6763bc3b9ae418f1f096337ccbe250adb15
Lelkes Balázs <balazs.lelkes89@gmail.com>
2021-05-18 11:42:21 +0200

    create files for testing

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import frontendRouter from './frontend.js';
+import backendRouter from './backend.js';
+
+const app = express();
+const port = 3000; // TODO env
+
+// FRONTEND
+app.use('/', frontendRouter);
+
+// BACKEND
+app.use('/api', backendRouter);
+
+// ERROR HANDLING
+app.use((err, req, res, next) => {
+  res.status(400).json({
+    error: err.message
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${ port }...`);
+});
